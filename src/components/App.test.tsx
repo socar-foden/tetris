88707ac6e3b5666d7ffaf84d604ed881c06c3913
brandon <zxczoxc125@naver.com>
2021-04-17 16:36:33 +0900

    # 앱 기본 구조 구현

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "./App";
 
 describe("test", () => {
-  it("test", () => {
+  beforeEach(() => {
     render(<App />);
+  });
+
+  describe("구성요소 테스트", () => {
+    it("게임영역, 시작버튼, level, rows, score 영역이 존재한다.", () => {
+      expect(screen.getByRole("game")).toBeInTheDocument();
+      expect(
+        screen.getByRole("section", { name: "level" })
+      ).toBeInTheDocument();
+      expect(screen.getByRole("section", { name: "rows" })).toBeInTheDocument();
+      expect(
+        screen.getByRole("section", { name: "score" })
+      ).toBeInTheDocument();
+      expect(screen.getByRole("button", { name: "start" })).toBeInTheDocument();
+    });
   });
 });
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <main>
+      <div role="game"></div>
+
+      <section role="section" aria-label="level"></section>
+      <section role="section" aria-label="rows"></section>
+      <section role="section" aria-label="score"></section>
+      <button aria-label="start">START GAME</button>
+    </main>
+  );
 };
 
 export default App;
