b2ba7cef5abfdc9c2295a46cbe0e4a985b22a66f
brandon <zxczoxc125@naver.com>
2021-04-21 08:43:39 +0900

    # block 타입 정의

---
+++ src/blocks/blocks.ts
@@ -0,0 +1,80 @@
+import { Space } from "../GameContext";
+
+const enum BlockType {
+  I,
+  O,
+  Z,
+  S,
+  J,
+  L,
+  T,
+}
+
+export interface Block {
+  position: number[][];
+  direction: number;
+  type: BlockType;
+}
+
+export const Block_I: Block = {
+  position: [[Space.block], [Space.block], [Space.block], [Space.block]],
+  direction: 0,
+  type: BlockType.I,
+};
+
+export const Block_O: Block = {
+  position: [
+    [Space.block, Space.block],
+    [Space.block, Space.block],
+  ],
+  direction: 0,
+  type: BlockType.O,
+};
+
+export const Block_Z: Block = {
+  position: [
+    [Space.block, Space.block, Space.empty],
+    [Space.empty, Space.block, Space.block],
+  ],
+  direction: 0,
+  type: BlockType.Z,
+};
+
+export const Block_S: Block = {
+  position: [
+    [Space.empty, Space.block, Space.block],
+    [Space.block, Space.block, Space.empty],
+  ],
+  direction: 0,
+  type: BlockType.S,
+};
+
+export const Block_J: Block = {
+  position: [
+    [Space.empty, Space.block],
+    [Space.empty, Space.block],
+    [Space.block, Space.block],
+  ],
+  direction: 0,
+  type: BlockType.J,
+};
+
+export const Block_L: Block = {
+  position: [
+    [Space.block, Space.empty],
+    [Space.block, Space.empty],
+    [Space.block, Space.block],
+  ],
+  direction: 0,
+  type: BlockType.L,
+};
+
+export const Block_T: Block = {
+  position: [
+    [Space.block, Space.block, Space.block],
+    [Space.empty, Space.block, Space.empty],
+    [Space.empty, Space.block, Space.empty],
+  ],
+  direction: 0,
+  type: BlockType.T,
+};
