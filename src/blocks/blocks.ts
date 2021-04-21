ac9777390522a00a192ebdad248ff98606f69edb
brandon <zxczoxc125@naver.com>
2021-04-21 19:38:23 +0900

    # 클래스 문법 제대로

--- src/blocks/blocks.ts
+++ src/blocks/blocks.ts
@@ -16,73 +16,109 @@ const enum BlockType {
 
 export class Block {
   protected _position: number[][];
-  protected _direction: number;
-  _type: BlockType;
+  _direction: number;
+  protected _type: BlockType;
 
   get position() {
     return this._position;
   }
-  get direction() {
-    return this._direction;
+  get type() {
+    return this._type;
   }
 }
 
 export class Block_I extends Block {
-  _position: [[Space.block], [Space.block], [Space.block], [Space.block]];
-  _direction: 0;
-  _type: BlockType.I;
+  constructor() {
+    super();
+
+    this._position = [
+      [Space.block],
+      [Space.block],
+      [Space.block],
+      [Space.block],
+    ];
+    this._direction = 0;
+    this._type = BlockType.I;
+  }
 }
 
 export class Block_O extends Block {
-  _position: [[Space.block, Space.block], [Space.block, Space.block]];
-  _direction: 0;
-  _type: BlockType.O;
+  constructor() {
+    super();
+
+    this._position = [
+      [Space.block, Space.block],
+      [Space.block, Space.block],
+    ];
+    this._direction = 0;
+    this._type = BlockType.O;
+  }
 }
 
 export class Block_Z extends Block {
-  _position: [
-    [Space.block, Space.block, Space.empty],
-    [Space.empty, Space.block, Space.block]
-  ];
-  _direction: 0;
-  _type: BlockType.Z;
+  constructor() {
+    super();
+
+    this._position = [
+      [Space.block, Space.block, Space.empty],
+      [Space.empty, Space.block, Space.block],
+    ];
+    this._direction = 0;
+    this._type = BlockType.Z;
+  }
 }
 
 export class Block_S extends Block {
-  _position: [
-    [Space.empty, Space.block, Space.block],
-    [Space.block, Space.block, Space.empty]
-  ];
-  _direction: 0;
-  _type: BlockType.S;
+  constructor() {
+    super();
+
+    this._position = [
+      [Space.empty, Space.block, Space.block],
+      [Space.block, Space.block, Space.empty],
+    ];
+    this._direction = 0;
+    this._type = BlockType.S;
+  }
 }
 
 export class Block_J extends Block {
-  _position: [
-    [Space.empty, Space.block],
-    [Space.empty, Space.block],
-    [Space.block, Space.block]
-  ];
-  _direction: 0;
-  _type: BlockType.J;
+  constructor() {
+    super();
+
+    this._position = [
+      [Space.empty, Space.block],
+      [Space.empty, Space.block],
+      [Space.block, Space.block],
+    ];
+    this._direction = 0;
+    this._type = BlockType.J;
+  }
 }
 
 export class Block_L extends Block {
-  _position: [
-    [Space.block, Space.empty],
-    [Space.block, Space.empty],
-    [Space.block, Space.block]
-  ];
-  _direction: 0;
-  _type: BlockType.L;
+  constructor() {
+    super();
+
+    this._position = [
+      [Space.block, Space.empty],
+      [Space.block, Space.empty],
+      [Space.block, Space.block],
+    ];
+    this._direction = 0;
+    this._type = BlockType.L;
+  }
 }
 
 export class Block_T extends Block {
-  _position: [
-    [Space.block, Space.block, Space.block],
-    [Space.empty, Space.block, Space.empty],
-    [Space.empty, Space.block, Space.empty]
-  ];
-  _direction: 0;
-  _type: BlockType.T;
+  constructor() {
+    super();
+
+    this._position = [
+      [Space.block, Space.block, Space.block],
+      [Space.empty, Space.block, Space.empty],
+      [Space.empty, Space.block, Space.empty],
+    ];
+    this._direction = 0;
+    this._type = BlockType.T;
+  }
 }
