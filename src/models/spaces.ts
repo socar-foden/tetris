d737c01f3fda511c0d31ae53e6ca6aa15790baf7
brandon <zxczoxc125@naver.com>
2021-04-22 14:47:26 +0900

    # space state 추가

--- src/models/spaces.ts
+++ src/models/spaces.ts
@@ -14,6 +14,7 @@ export class Space_Empty extends Space {
   constructor() {
     super();
 
+    this._state = SpaceState.empty;
     this._color = "white";
     this._border = "1px dotted black";
   }
@@ -23,6 +24,7 @@ export class Space_Block extends Space {
   constructor(color: string) {
     super();
 
+    this._state = SpaceState.block;
     this._color = color;
     this._border = "1px solid black";
   }
@@ -32,6 +34,7 @@ export class Space_Disabled extends Space {
   constructor() {
     super();
 
+    this._state = SpaceState.disabled;
     this._color = "grey";
     this._border = "1px solid black";
   }
