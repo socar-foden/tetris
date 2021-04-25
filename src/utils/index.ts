b3a54c1fd94d606e990d7dca5c72f97f0749fa1d
brandon <zxczoxc125@naver.com>
2021-04-26 08:32:30 +0900

    # 새 인스턴스 할당

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,7 +9,7 @@ import {
   Block_T,
   Block_Z,
 } from "../models/blocks";
-import { Space, SpaceState, Space_Empty } from "../models/spaces";
+import { Space, SpaceState, Space_Block, Space_Empty } from "../models/spaces";
 
 export const getRandomBlock = (): Block => {
   const blockClassList: Block[] = [
@@ -32,24 +32,21 @@ interface Location {
 }
 
 export const getSpaceList = (
-  startLocation: Location,
+  location: Location,
   block: Block,
   spaceList: Space[][]
 ): Space[][] => {
   const cloned = _.cloneDeep(spaceList);
-  const { d_1, d_2 } = startLocation;
-  const { position, border, color } = block;
+  const { d_1, d_2 } = location;
+  const { position, color } = block;
 
   const range_d_2 = _.range(d_2, d_2 + position[0].length);
   const range_d_1 = _.range(d_1, d_1 + position.length);
 
   _.forEach(range_d_1, (d1, i) =>
     _.forEach(range_d_2, (d2, j) => {
-      const target = cloned[d1][d2];
-
       if (_.isEqual(position[i][j]._state, SpaceState.block)) {
-        target._border = border;
-        target._color = color;
+        cloned[d1][d2] = new Space_Block(color);
       }
     })
   );
