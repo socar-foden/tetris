2bc69fb3f95f6603d9513623631e0d813c2322ec
brandon <zxczoxc125@naver.com>
2021-04-30 18:43:00 +0900

    # 기본 회전 로직 구현

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import GameContext, { gameState } from "../../GameContext";
+import GameContext, { gameState, Progress } from "../../GameContext";
+import { Block_Empty } from "../../models/blocks";
+import { getRotatedBlock, getSpaceList } from "../../utils";
 import Game from "../Game/Game";
 import GlobalStyle from "../GlobalStyle";
 import Info from "../Info/Info";
@@ -8,9 +10,41 @@ import S from "./App.style";
 const App: React.FC = () => {
   const [state, setState] = useState(gameState);
   state.setGameState = setState;
+  const { progress } = state;
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    console.log(e);
+    console.log(e.key);
+
+    const { key } = e;
+
+    if (progress === Progress.proceeding) {
+      if (key === "ArrowUp") {
+        setState((prev) => {
+          return {
+            ...prev,
+            spaceList: getSpaceList(
+              prev.currentLocation,
+              new Block_Empty(prev.currentBlock._position),
+              prev.spaceList
+            ),
+          };
+        });
+
+        setState((prev) => {
+          const currentBlock = getRotatedBlock(prev.currentBlock);
+
+          return {
+            ...prev,
+            currentBlock,
+            spaceList: getSpaceList(
+              prev.currentLocation,
+              currentBlock,
+              prev.spaceList
+            ),
+          };
+        });
+      }
+    }
   };
 
   return (
