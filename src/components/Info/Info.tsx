bd67b26fd5fa6994ca1ed1fc797e13b77a5bf009
brandon <zxczoxc125@naver.com>
2021-05-06 21:56:27 +0900

    # 불필요 코드 삭제

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import _ from "lodash";
 import fp from "lodash/fp";
-import GameContext, { gameState, Progress } from "../../GameContext";
+import GameContext, { Progress } from "../../GameContext";
 import S from "./Info.style";
 import Game_S from "../Game/Game.style";
 import When from "../When/When";
